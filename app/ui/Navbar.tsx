89c2b90cbe75af9545b41da7533a74b3425ac76e
Anurag <anurag.angalcs@gmail.com>
2024-01-01 08:01:19 +0530

    vercel analytics added

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="py-6 pl-4 sticky top-0 z-10 border-b border-black flex font-[#121212]">
+    <nav className="py-6 pl-4 sticky top-0 z-10 border-b border-black font-[#121212]">
       <div className="text-5xl md:text-8xl font-thin">
         <Link href="/" className={`${redRose.className} no-underline`}>
           Anurag&nbsp;Angal
